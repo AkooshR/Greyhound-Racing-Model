3f1f2627725a4979a578432d845159e12eeb823b
AkooshR <akashroy8106@gmail.com>
2026-01-28 18:48:24 +1100

    Update download.py

--- backtesting/download.py
+++ backtesting/download.py
@@ -1,5 +1,7 @@
 import betfairlightweight
 import os
+import bz2
+import json
 import pandas as pd
 from dotenv import load_dotenv
 from pathlib import Path
@@ -14,19 +16,19 @@ trading.login_interactive()
 
 #Getting all the available historic data in May 2020, and filtering for AvB markets (MATCH_BET). Outputs the file pathways for each AvB market!
 #the for loop then checks if a file has already been downloaded. if not, it attempts to download it.
-AvB_list = trading.historic.get_file_list(
-    sport='Greyhound Racing',
-    plan='Pro Plan',
-    from_day='1',
-    from_month='5',
-    from_year='2020',
-    to_day='31',
-    to_month='5',
-    to_year='2020',
-    event_id=None,
-    event_name=None,
-    market_types_collection='MATCH_BET'
-)
+#AvB_list = trading.historic.get_file_list(
+#    sport='Greyhound Racing',
+#    plan='Pro Plan',
+#    from_day='1',
+#    from_month='5',
+#    from_year='2020',
+#    to_day='31',
+#    to_month='5',
+#    to_year='2020',
+#    event_id=None,
+#    event_name=None,
+#    market_types_collection='MATCH_BET'
+#)
 #for file in AvB_list:
 #    if not Path(f'backtestdata/{file.split('/')[8]}').exists():
 #        trading.historic.download_file(file,"backtestdata")
