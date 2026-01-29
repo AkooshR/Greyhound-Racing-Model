d1db01f663bc5bf411d9f6430cb9afa1f9f3a068
AkooshR <akashroy8106@gmail.com>
2026-01-29 15:38:01 +1100

    Update download.py

--- backtesting/download.py
+++ backtesting/download.py
@@ -16,7 +16,9 @@ trading.login_interactive()
 
 #Getting all the available historic data in May 2020, and filtering for AvB markets (MATCH_BET). Outputs the file pathways for each AvB market!
 #the for loop then checks if a file has already been downloaded. if not, it attempts to download it.
-AvB_list = trading.historic.get_file_list(
+
+if input("Continue to download AvB files (type 'q'): ") == 'q':
+    AvB_list = trading.historic.get_file_list(
     sport='Greyhound Racing',
     plan='Pro Plan',
     from_day='1',
@@ -28,9 +30,7 @@ AvB_list = trading.historic.get_file_list(
     event_id=None,
     event_name=None,
     market_types_collection='MATCH_BET'
-)
-
-if input("Continue to download files (type 'q'): ") == 'q':
+    )
     for file in AvB_list:
         MAX_RETRIES = 3
         for attempt in range(MAX_RETRIES):
@@ -45,32 +45,32 @@ if input("Continue to download files (type 'q'): ") == 'q':
                     print(f"Failed to download {file} after {MAX_RETRIES} attempts")
                 continue
 
-win_list = trading.historic.get_file_list(
-    sport='Greyhound Racing',
-    plan='Pro Plan',
-    from_day='1',
-    from_month='5',
-    from_year='2020',
-    to_day='31',
-    to_month='5',
-    to_year='2020',
-    event_id=None,
-    event_name=None,
-    market_types_collection='WIN'
-)
-json_upload = {}
-json_file = open('win_markets_may.json','w')
-for file in win_list:
-    trading.historic.download_file(file,"winmarket")
-    line1 = json.loads(next(bz2.open(Path(f'winmarket/{file.split('/')[8]}'), "rt")))
-    date = line1['mc'][0]['marketDefinition']['marketTime'][:10]
-    eventid = line1['mc'][0]['marketDefinition']['eventId']
-    runner_list = []
-    for runner in line1['mc'][0]['marketDefinition']['runners']:
-        runner_list.append(runner['name'].split('.')[1].strip())
-    race_length = line1['mc'][0]['marketDefinition']['name'].split()[1]
-    json_upload.setdefault(eventid, []).append([date, race_length, runner_list])
-    print(json_upload)
-    os.remove(Path(f'winmarket/{file.split('/')[8]}'))
-
-json.dump(json_upload,json_file,indent=4)
+if input("Continue to generate win markets json file (type 'q'): ") == 'q':
+    win_list = trading.historic.get_file_list(
+        sport='Greyhound Racing',
+        plan='Pro Plan',
+        from_day='1',
+        from_month='5',
+        from_year='2020',
+        to_day='31',
+        to_month='5',
+        to_year='2020',
+        event_id=None,
+        event_name=None,
+        market_types_collection='WIN'
+    )
+    json_upload = {}
+    JSON_PATH = 'win_markets_may.json'
+    for file in win_list:
+        trading.historic.download_file(file,"winmarket")
+        line1 = json.loads(next(bz2.open(Path(f'winmarket/{file.split('/')[8]}'), "rt")))
+        date = line1['mc'][0]['marketDefinition']['marketTime'][:10]
+        eventid = line1['mc'][0]['marketDefinition']['eventId']
+        runner_list = []
+        for runner in line1['mc'][0]['marketDefinition']['runners']:
+            runner_list.append(runner['name'].split('.')[1].strip())
+        race_length = line1['mc'][0]['marketDefinition']['name'].split()[1]
+        json_upload.setdefault(eventid, []).append([date, race_length, runner_list])
+        print(json_upload)
+        os.remove(Path(f'winmarket/{file.split('/')[8]}'))
+    json.dump(json_upload,open(JSON_PATH,'w'),indent=4)
